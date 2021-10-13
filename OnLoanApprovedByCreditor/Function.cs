393722ae8e5298e15cfd2920a585dd531db3b57a
PravinDalavi <pravin.dalavi@gmail.com>
2021-10-13 11:37:29 +0530

    Updated Code

--- OnLoanApprovedByCreditor/Function.cs
+++ OnLoanApprovedByCreditor/Function.cs
@@ -39,23 +39,19 @@ namespace OnLoanApprovedByCreditor
             string json = JsonConvert.SerializeObject(input);
             StringContent data = new StringContent(json, Encoding.UTF8, "application/json");
 
-            //Derived obj = new Derived();
-
-            //string a = obj.GetToken();
-
-            string CallBackUrl = "https://g9yh14f7ve.execute-api.ap-south-1.amazonaws.com/Authorizeddev/loanapproval/banker";
+            string CallBackUrl = Environment.GetEnvironmentVariable("LoanApprovalURL");
+            //string CallBackUrl = "https://g9yh14f7ve.execute-api.ap-south-1.amazonaws.com/Authorizeddev/loanapproval/banker";
             string url = CallBackUrl + "/" + input.External_ID.ToString();
             var client = new HttpClient();
 
             Uri myURI = new Uri(url);
             //Need Banker Authorization access token
-            //string AccessToken = _configuration.GetSection("BankerAccessToken").Value.ToString();
-            client.DefaultRequestHeaders.Add("Authorization", "eyJraWQiOiJrSURWK2lTS0RvMU5mMGJHbjhSUlpiMnNIdE1jYVJmS2JZMDB6eWVXM29rPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI5ZWIzYzZlNy1lMjM5LTRlZTQtYWUxOC1mMjNiNmU0NzhkN2UiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6ImF3cy5jb2duaXRvLnNpZ25pbi51c2VyLmFkbWluIHBob25lIG9wZW5pZCBwcm9maWxlIGVtYWlsIiwiYXV0aF90aW1lIjoxNjMzNjgwMjQ5LCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuYXAtc291dGgtMS5hbWF6b25hd3MuY29tXC9hcC1zb3V0aC0xX2hFamF6OGdxcyIsImV4cCI6MTYzMzc2NjY0OSwiaWF0IjoxNjMzNjgwMjQ5LCJ2ZXJzaW9uIjoyLCJqdGkiOiJiZDc0OGIwMC1jMjEzLTRhMWEtODY4MS04ZGM4ZDhkMzJhZTkiLCJjbGllbnRfaWQiOiIzNHNvNGplcjQ0dTJwMzlmZmpxYmxuN2N2dCIsInVzZXJuYW1lIjoicHJhdmluIn0.QYeynIjFbvGH0l-ydVRFbLOI2M3XP_EIOx9mWXKqKizxpffzSiZE9i0ciENKF2OgcIaJQjgTH_eNGdpwm3F76mIlFyedSLSAKMS6a7UNNWXbpkqOPp9ZcrYPmj2Y3NxqkwamFp9rWKu5sE2rdwP5pp_fQnRF3Cxwa4UGGppFqapqQiWkkZwDQSuKpASy9rGhRpQjCbcoblFKnbWwYi7nAmRlu7cDmR3GWpqApOAdX3j6hD3AlHm82lWJt78uvs8SiafCRh22mM3D9ASZs9IguKTuj5dr3cXi-amtjKrchNRkoLCY4LJNcD9upL_sKKqjMhU-WMmFXU-KQ1iQBktFbQ");
+            client.DefaultRequestHeaders.Add("Authorization", "eyJraWQiOiJrSURWK2lTS0RvMU5mMGJHbjhSUlpiMnNIdE1jYVJmS2JZMDB6eWVXM29rPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI5ZWIzYzZlNy1lMjM5LTRlZTQtYWUxOC1mMjNiNmU0NzhkN2UiLCJldmVudF9pZCI6IjRjNDUwOTk0LWRlN2QtNDE5Ny04YWJhLWRmMmU2YTFkZTRlOSIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4gcGhvbmUgb3BlbmlkIHByb2ZpbGUgZW1haWwiLCJhdXRoX3RpbWUiOjE2MzQwOTc1NDQsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5hcC1zb3V0aC0xLmFtYXpvbmF3cy5jb21cL2FwLXNvdXRoLTFfaEVqYXo4Z3FzIiwiZXhwIjoxNjM0MTgzOTQ0LCJpYXQiOjE2MzQwOTc1NDQsInZlcnNpb24iOjIsImp0aSI6ImM3MjQ2YTdmLWYwYTAtNGMzNS04N2Y3LWMzNWVhNDNiMmU4MSIsImNsaWVudF9pZCI6IjM0c280amVyNDR1MnAzOWZmanFibG43Y3Z0IiwidXNlcm5hbWUiOiJwcmF2aW4ifQ.w9o8eRY75Fv0MtFCjeBMGa9RHiKed1p-bPxhvAn4DF38waxWRVZA3X5UgvrzkKsdxs4C43zgFOLVdwtwErS6Z5AqltWOkrg5fU2QYYmHhqGnbmPYSSkVmUA602H8cFptoZX5hpcSfIopCPn4O0Sbfh1BLGY5kzhfhIUvFbQSH3Iy6MPcsGlUwFTKSIyCvFXEYDZgSGsV1NmUCpsWRv3rkwuA1ioMr6sSimjbX3gCvZNEp8fur8b29GPmInwd2W5Y-aSyUTLSXRfMKL-YAY08UCiDg9OnxkU1l2SIsa830BhXQaTqN09nSyfGs5fuDps_NgYlwVmEeE7w_hfuwqFScA");
 
             var response = await client.PutAsync(myURI.AbsoluteUri, data);
 
         }
     }
 
-    
+
 }
